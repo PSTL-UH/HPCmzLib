61b895676d13d55b7e4e34c067f168f7bca64d59
Edgar Gabriel <egabriel@central.uh.edu>
2019-03-06 15:21:00 -0600

    checkpoint for making the PeriodicTableReader work.
    Also adding the elements.dat file that will be used
    to populate the Periodic Table.
    
    Signed-off-by: Edgar Gabriel <egabriel@central.uh.edu>

--- UsefulProteomicsDatabases/PeriodicTableLoader.cpp
+++ UsefulProteomicsDatabases/PeriodicTableLoader.cpp
@@ -2,72 +2,84 @@
 #include "../Chemistry/Element.h"
 #include "../Chemistry/PeriodicTable.h"
 
-using namespace Chemistry;
+#include <iostream>
+#include <fstream>
+
+//using namespace Chemistry;
 
 namespace UsefulProteomicsDatabases {
 
     void PeriodicTableLoader::Load(const std::string &elementLocation) {
 //C# TO C++ CONVERTER NOTE: The following 'using' block is replaced by its C++ equivalent:
 //ORIGINAL LINE: using (StreamReader sr = new StreamReader(elementLocation))
-    {
-            StreamReader sr = StreamReader(elementLocation);
-            std::string line = sr.ReadLine();
-            while (!line.find("Atomic Number") != std::string::npos) {
-                line = sr.ReadLine();
+#ifdef ORIG
+        StreamReader sr = StreamReader(elementLocation);
+        std::string line = sr.ReadLine();
+        while (!line.find("Atomic Number") != std::string::npos) {
+            line = sr.ReadLine();
+        }
+#endif
+        std::ifstream sr(elementLocation);
+        std::string line;
+        bool found=false;
+        while ( !found ) {
+            getline(sr, line);
+            //parse for "Atomic Number");
+        }
+        
+        auto prevAtomicNumber = -1;
+        Element *element = new Element("fake", prevAtomicNumber, -1);
+        do {
+            int atomicNumber = Convert::ToInt32(Regex::Match(line, R"(\d+)")->Value);
+            
+            getline(sr, line);
+            std::string atomicSymbol = Regex::Match(line, R"([A-Za-z]+$)")->Value;
+            
+            getline(sr, line);
+            int massNumber = Convert::ToInt32(Regex::Match(line, R"(\d+)")->Value);
+            
+            getline(sr, line);
+            double atomicMass = Convert::ToDouble(Regex::Match(line, R"([\d\.]+)")->Value);
+            
+            getline(sr, line);
+            double abundance;
+            if (Regex::Match(line, R"([\d\.]+)")->Success) {
+                abundance = Convert::ToDouble(Regex::Match(line, R"([\d\.]+)")->Value);
             }
-            auto prevAtomicNumber = -1;
-            Element *element = new Element("fake", prevAtomicNumber, -1);
-            do {
-                int atomicNumber = Convert::ToInt32(Regex::Match(line, R"(\d+)")->Value);
-
-                line = sr.ReadLine();
-                std::string atomicSymbol = Regex::Match(line, R"([A-Za-z]+$)")->Value;
-
-                line = sr.ReadLine();
-                int massNumber = Convert::ToInt32(Regex::Match(line, R"(\d+)")->Value);
-
-                line = sr.ReadLine();
-                double atomicMass = Convert::ToDouble(Regex::Match(line, R"([\d\.]+)")->Value);
-
-                line = sr.ReadLine();
-                double abundance;
-                if (Regex::Match(line, R"([\d\.]+)")->Success) {
-                    abundance = Convert::ToDouble(Regex::Match(line, R"([\d\.]+)")->Value);
-                }
-                else {
-                    sr.ReadLine();
-                    sr.ReadLine();
-                    sr.ReadLine();
-                    line = sr.ReadLine();
-                    continue;
-                }
-
-                line = sr.ReadLine();
-                double averageMass;
-                if (Regex::Match(line, R"(\[)")->Success) {
-                    double averageMass1 = Convert::ToDouble(Regex::Match(line, R"((?<=\[)[\d\.]+)")->Value);
-                    auto kkajsdf = Regex::Match(line, R"((?<=,)[\d\.]+)")->Value;
-                    double averageMass2 = Convert::ToDouble(kkajsdf);
-                    averageMass = (averageMass1 + averageMass2) / 2;
-                }
-                else {
-                    averageMass = Convert::ToDouble(Regex::Match(line, R"([\d\.]+)")->Value);
-                }
-
-                if (atomicNumber != prevAtomicNumber) {
-                    element = new Element(atomicSymbol, atomicNumber, averageMass);
-                    PeriodicTable::Add(element);
-                }
-
-                element->AddIsotope(massNumber, atomicMass, abundance);
-
-                sr.ReadLine();
-                sr.ReadLine();
-                line = sr.ReadLine();
-                prevAtomicNumber = atomicNumber;
-            } while (line.find("Atomic Number") != std::string::npos);
-
+            else {
+                getline(sr, line);
+                getline(sr, line);
+                getline(sr, line);
+                getline(sr, line);
+                continue;
+            }
+            
+            getline(sr, line);
+            double averageMass;
+            if (Regex::Match(line, R"(\[)")->Success) {
+                double averageMass1 = Convert::ToDouble(Regex::Match(line, R"((?<=\[)[\d\.]+)")->Value);
+                auto kkajsdf = Regex::Match(line, R"((?<=,)[\d\.]+)")->Value;
+                double averageMass2 = Convert::ToDouble(kkajsdf);
+                averageMass = (averageMass1 + averageMass2) / 2;
+            }
+            else {
+                averageMass = Convert::ToDouble(Regex::Match(line, R"([\d\.]+)")->Value);
+            }
+            
+            if (atomicNumber != prevAtomicNumber) {
+                element = new Element(atomicSymbol, atomicNumber, averageMass);
+                PeriodicTable::Add(element);
+            }
+            
+            element->AddIsotope(massNumber, atomicMass, abundance);
+            
+            getline(sr, line);
+            getline(sr, line);
+            getline(sr, line);
+            prevAtomicNumber = atomicNumber;
+        } while (line.find("Atomic Number") != std::string::npos);
+        
 //C# TO C++ CONVERTER TODO TASK: A 'delete element' statement was not added since element was passed to a method or constructor. Handle memory management manually.
-    }
+        sr.close();
     }
 }
--- UsefulProteomicsDatabases/PeriodicTableLoader.h
+++ UsefulProteomicsDatabases/PeriodicTableLoader.h
@@ -19,6 +19,7 @@
 // You should have received a copy of the GNU Lesser General Public
 // License along with UsefulProteomicsDatabases. If not, see <http://www.gnu.org/licenses/>.
 
+#include "../Chemistry/Chemistry.h"
 using namespace Chemistry;
 
 namespace UsefulProteomicsDatabases {
