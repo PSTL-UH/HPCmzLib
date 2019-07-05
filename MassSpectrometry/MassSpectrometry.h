2b2b3da757fccbb249b924967f4674cfaae1703c
Edgar Gabriel <egabriel@central.uh.edu>
2019-07-05 10:55:51 -0500

    MassSpectrometry: add a new header file.
    Signed-off-by: Edgar Gabriel <egabriel@central.uh.edu>

---
+++ MassSpectrometry/MassSpectrometry.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "DeconvolutionFeature.h"
+#include "DeconvolutionFeatureWithMassesAndScans.h"
+#include "GeneratedMzSpectrum.h"
+#include "GeneratedPeak.h"
+#include "IIdentifications.h"
+#include "IMsDataFile.h"
+#include "IMsDynamicDataFile.h"
+#include "IMsStaticDataFile.h"
+#include "MsDataFile.h"
+#include "SourceFile.h"
+#include "SummedMsDataFile.h"
+
