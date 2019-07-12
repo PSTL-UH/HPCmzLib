833dc37b7c8c78a3a8f8d7d4ad182afc7777f399
Nick Biddle <nrbiddle@gmail.com>
2019-07-12 12:03:34 -0500

    Added HeapSortPairs and CreateHeap functions.  Also changed SortPairs function to use HeapSortPairs rather than bubble sort

--- include/Sort.h
+++ include/Sort.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <algorithm>
 #include <vector>
 
 class Sort{
@@ -7,7 +7,8 @@ public:
 
     static void SortPairs(std::vector<double> &keys,
                           std::vector<double> &values, int size) {
-        return BubbleSortPairs ( keys, values, size );
+        // return BubbleSortPairs ( keys, values, size );
+        return HeapSortPairs ( keys, values, size );
     }
 
     static void BubbleSortPairs(std::vector<double> &keys,
@@ -27,5 +28,39 @@ public:
                 }
             }
         }
-    }  
+    }
+
+    static void CreateHeap(std::vector<double> &keys, std::vector<double> &values, int size, int i) { 
+        int largest = i;
+        int left = 2*i + 1; 
+        int right = 2*i + 2; 
+      
+        if (left < size && keys[left] > keys[largest]) {
+            largest = left; 
+        }
+      
+        if (right < size && keys[right] > keys[largest]) {
+            largest = right; 
+        }
+      
+        if (largest != i) 
+        { 
+            std::swap(keys[i], keys[largest]);
+            std::swap(values[i], values[largest]);  
+            CreateHeap(keys, values, size, largest); 
+        } 
+    } 
+
+    static void HeapSortPairs(std::vector<double> &keys, std::vector<double> &values, int size) {
+        // Sort an array of values based on the keys provided using the Heap Sort algotithm.
+        for (int i = size / 2 - 1; i >= 0; i--) 
+            CreateHeap(keys, values, size, i); 
+
+        for (int i = size-1; i >= 0; i--) 
+        { 
+            std::swap(keys[0], keys[i]); 
+            std::swap(values[0], values[i]); 
+            CreateHeap(keys, values, i, 0); 
+        } 
+    }
 };
