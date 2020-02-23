7a96d69e915be7f00c023f48917e83098885c9e4
Edgar Gabriel <egabriel@central.uh.edu>
2020-02-23 09:12:38 -0600

    update ParameterBounds to implement the full interface required.
    Signed-off-by: Edgar Gabriel <egabriel@central.uh.edu>

--- FlashLFQ/BoundedNelderMeadOptimizer/ParameterBounds.h
+++ FlashLFQ/BoundedNelderMeadOptimizer/ParameterBounds.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 
-// This is just a temporary placeholder until its clear what else we need from this class.
-// It is based of the SharpLearning.Optimization.ParameterSpecs::MinMaxParameterSpec class.
+#include "Transform.h"
+#include "ParameterType.h"
 
 namespace FlashLFQ
 {
@@ -10,20 +10,22 @@ namespace FlashLFQ
         /// Type of the parameter
         /// </summary>
         class ParameterBounds {
+        private:
+            Transform const transform;            
+            const ParameterType parameterType;
+
         public:
             double Min=0, Max=0;
 
-            ParameterBounds (double min, double max) {
+            ParameterBounds (double min, double max, Transform trans = Transform::Linear, ParameterType paramType = ParameterType::Continuous): transform(trans), parameterType(paramType) {
                 if ( min > max ) {
                     //uff
                 }
                 Min = min;
                 Max = max;
+
             }
             
-            //Transform transform=Transform::Linear; 
-            //ParameterType parameterType = ParameterType::Continuous;
-
             //double SampleValue(ParameterSampler *sampler) override;
         };
     }
