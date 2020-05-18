1c1a719a3eb0e458ec89ec0d3a970f12aa356f03
Edgar Gabriel <egabriel@central.uh.edu>
2020-05-18 13:56:48 -0500

    InitiatorMethionineBehavior.h: add a ToString method
    Signed-off-by: Edgar Gabriel <egabriel@central.uh.edu>

--- Proteomics/ProteolyticDigestion/InitiatorMethionineBehavior.h
+++ Proteomics/ProteolyticDigestion/InitiatorMethionineBehavior.h
@@ -1,5 +1,7 @@
 ﻿#pragma once
 
+#include <string>
+
 namespace Proteomics
 {
     namespace ProteolyticDigestion
@@ -11,5 +13,26 @@ namespace Proteomics
             Cleave,
             Variable
         };
+
+        static std::string InitiatorMethionineBehaviorToString( InitiatorMethionineBehavior i)
+        {
+            std::string s;
+            if ( i == InitiatorMethionineBehavior::Undefined ) {
+                s = "Undefined";
+            }
+            else if ( i == InitiatorMethionineBehavior::Retain ) {
+                s = "Retain";
+            }
+            else if ( i == InitiatorMethionineBehavior::Cleave  ) {
+                s = "Cleave";
+            }
+            else if ( i == InitiatorMethionineBehavior::Variable) {
+                s = "Variable";
+            }
+            
+            return s;
+        }
     }
+
+    
 }
