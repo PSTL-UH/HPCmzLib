3c17be8f4d46a30c38eca10874f325ddf68957c8
Edgar Gabriel <egabriel@central.uh.edu>
2020-09-20 14:08:42 -0500

    MatchedFragmentIon: further adjustment to Pack/Unpack interfaces
    TestSerialization now also has the option of using MPI for one tests.
    use mpiCC for compilation in that case.
    
    Signed-off-by: Edgar Gabriel <egabriel@central.uh.edu>

--- Proteomics/Fragmentation/MatchedFragmentIon.h
+++ Proteomics/Fragmentation/MatchedFragmentIon.h
@@ -46,9 +46,47 @@ namespace Proteomics
 
             int GetHashCode();
 
+            /// <summary>
+            /// Pack a MatchedFragmentIon into a character buffer.
             /// Required for Communication Operations in HPCMetaMorpheus
-            static int Pack ( char *buf, size_t *buf_size, MatchedFragmentIon *MaF);
-            static void Unpack ( char *buf, size_t buf_size, MatchedFragmentIon **newMaF );
+            ///
+            /// Arguments:
+            /// buf :     INOUT buffer used for packing
+            /// buf_size: IN size of the allocated buffer provided by the upper layer
+            ///           OUT size of required buffer if not large enough (return value -1)
+            ///               or number of bytes used for packgin (return value > 0)
+            /// MaF :     IN (vector of) MatchedFragmentIon(s) to pack
+            ///
+            /// Return value:
+            ///   -1 : input buffer was not large enough. buf_size will contain the required number
+            ///        of bytes in this case
+            ///   >0 : packing successful, number of bytes used up.
+            /// </summary>
+            static int Pack ( char *buf, size_t &buf_size, MatchedFragmentIon *MaF);
+            static int Pack ( char *buf, size_t &buf_size, const std::vector<MatchedFragmentIon *> &MaF);
+
+            /// <summary>
+            /// Functionality used to reconstruct a MatchedFragmentIon based on a
+            /// packed buffer.
+            ///
+            /// Arguments
+            /// ---------
+            /// buf:      IN input character buffer
+            /// buf_size: IN size of input buffer
+            /// len:      OUT number of bytes used for unpacking 'count' elements
+            /// newMaF:   OUT (vector of) new MatchedFragmentIon(s) .
+            /// count:    IN how many elements to unpack.
+            ///           default : -1 (until end of buffer is reached)
+            /// </summary>
+            static void Unpack ( char *buf, size_t buf_size, size_t &len,
+                                 std::vector<MatchedFragmentIon *> &newMaF,
+                                 int count = -1 );
+            static void Unpack ( char *buf, size_t buf_size, size_t &len,
+                                 MatchedFragmentIon **newMaF );
+            static void Unpack ( std::string line, size_t &len,
+                                 MatchedFragmentIon **newMaF );
+
+
         };
     }
 }
