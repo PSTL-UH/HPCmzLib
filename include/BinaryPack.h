caf9cfdd24b2e9ec6595b420ed9d6b0a9817cc52
Edgar Gabriel <egabriel@central.uh.edu>
2021-07-15 07:45:12 -0500

    BinaryPack: make the API more userfriendly
    a small 2-byte header will help to detect memory corruption more easily.
    
    Signed-off-by: Edgar Gabriel <egabriel@central.uh.edu>

--- include/BinaryPack.h
+++ include/BinaryPack.h
@@ -3,16 +3,20 @@
 #include <cstdint>
 #include <string>
 #include <string.h>
-
+#include <iostream>
 
 class BinaryPack {
+private:
+    static const int16_t BinaryPackHeader=0xABCD;
+    
 public:
+    static const int LineStartOffset = sizeof(int16_t)+sizeof(int);
+
     static size_t PackFloat ( char *buf, const float &val)
     {
         memcpy ( buf, &val, sizeof(float));
         return sizeof(float);
     }
-
     static size_t UnpackFloat ( char* buf, float &val)
     {
         memcpy (&val, buf, sizeof(float));
@@ -24,7 +28,6 @@ public:
         memcpy ( buf, &val, sizeof(double));
         return sizeof(double);
     }
-
     static size_t UnpackDouble ( char *buf, double &val)
     {
         memcpy ( &val, buf, sizeof(double));
@@ -36,11 +39,21 @@ public:
         memcpy ( buf, &val, sizeof(int));
         return sizeof(int);
     }
-
     static size_t UnpackInt ( char *buf, int &val)
     {
         memcpy ( &val, buf, sizeof(int));
         return sizeof(int);
+    }
+
+    static size_t PackBool ( char *buf, const bool &val)
+    {
+        memcpy ( buf, &val, sizeof(bool));
+        return sizeof(bool);
+    }
+    static size_t UnpackBool ( char *buf, bool &val)
+    {
+        memcpy ( &val, buf, sizeof(bool));
+        return sizeof(bool);
     }
     
     static size_t PackString ( char *buf, const std::string &val)
@@ -68,16 +81,47 @@ public:
     {
         std::vector<char *> tvec;
         size_t pos=0;
+        int16_t header;
         while ( pos < buf_len ) {
             int linelen, retlen;
 
             char *tpos = &buf[pos];
             tvec.push_back(tpos);
-            retlen = BinaryPack::UnpackInt (buf+pos, linelen);
+            memcpy(&header, buf+pos, sizeof(int16_t));
+            if ( header != BinaryPackHeader ) {
+                std::cout <<"Memory corruption detected in BinaryPack::SplitLines\n";
+                tvec.clear();
+                return tvec;
+            }
+            retlen = BinaryPack::UnpackInt (buf+pos+sizeof(int16_t), linelen);
             pos += linelen;            
         }
         
         return tvec;
+    }
+
+    static void SetLineLength( char *buf, const int &len)
+    {
+        // Copy first a 2byte header to the beginning  of the line.
+        // Allows to identify memory corruption easier.
+        memcpy(buf, &BinaryPackHeader, sizeof(int16_t));
+        memcpy(buf+sizeof(int16_t), &len, sizeof(int));
+    }
+
+    static int GetLineLength( char *buf, int &linelen)
+    {
+        int retlen;
+        int16_t header;
+        int pos=0;
+        memcpy(&header, buf+pos, sizeof(int16_t));
+        if ( header != BinaryPackHeader ) {
+            std::cout <<"Memory corruption detected in BinaryPack::GetLineLength\n";
+            return -1;
+        }
+        pos += sizeof(int16_t);
+        retlen = BinaryPack::UnpackInt (buf+pos, linelen);
+        
+        return pos+retlen;
     }
     
 };
